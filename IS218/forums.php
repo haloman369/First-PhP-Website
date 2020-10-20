5bd771a82a34d5132747ca0afd3968c959cb8741
Daniel D. Drechsel <ddd9@afsconnect2.njit.edu>
2020-10-20 15:43:58 -0400

    adding files

---
+++ IS218/forums.php
@@ -0,0 +1,118 @@
+
+<nav>
+    <ul>     
+        <li>
+            <a href="index.php">Home</a>
+        </li>
+        <li>
+            <a href="login.php">Login</a>
+        </li>
+       <li>
+            <a href="register.php">Register</a>
+        </li>
+    </ul>
+</nav>
+
+<h4>Forum</h4>
+<form method="POST">
+	
+	<label for="questionName">Question Name
+	<input type="questionName" id="questionName" name="questionName" autocomplete="off" />
+	</label>
+	
+	<label for="questionBody">Question Body
+	<input type="questionBody" id="questionBody" name="questionBody" autocomplete="off" />
+	</label>
+
+	<label for="questionSkills">Question Skills
+	<input type="questionSkills" id="questionSkills" name="questionSkills" autocomplete="off" />
+	</label>
+
+	
+	<input type="submit" name="forum" value="Forum"/>
+</form>
+
+<?php
+
+
+if(isset($_POST["forum"])){
+	if(isset($_POST["questionName"]) && isset($_POST["questionBody"]) && isset($_POST["questionSkills"])  ){
+
+		$questionName = $_POST["questionName"];
+		$questionBody = $_POST["questionBody"];
+		$questionSkills = $_POST["questionSkills"];
+			
+			$errors = [];
+			$skills = explode(',', $questionSkills);
+
+
+			
+			if(!strrpos ($questionSkills , ',' ) )
+			{
+
+				array_push($errors, "Needs a comma to seperaet entries");
+
+			}
+			
+
+			if(count($skills) < 2){
+
+				array_push($errors, "You need to enter 2 or more skills");
+			}
+
+			if(empty($questionName)){
+
+				array_push($errors, "Must enter a question name");
+
+			}
+
+			if(strlen($questionName) < 3 ){
+
+				array_push($errors, "Question name must be 3 or more characters");
+
+			}
+
+			if(empty($questionBody)){
+
+				array_push($errors, "Must have text in your question body");
+
+			}
+
+			if(strlen($questionBody) > 500){
+
+				array_push($errors, "Question body must be less than 500 characters, Body is " . strlen($questionBody) . " characters");
+
+			}
+
+			if(empty($questionSkills)){
+
+				array_push($errors, "Must enter skills");
+
+			}
+			
+			if(count($errors) > 0){
+
+				echo var_export($errors);
+				die();
+
+			}
+	}
+
+	echo "Question Name: " . $questionName . "<br>";
+	echo "Question Body: ". $questionBody . "<br>";
+	
+	foreach ($skills as $skill) {
+
+		echo $skill . "<br>";
+
+	}
+
+	for($i = 0; $i<count($skills); $i++){
+
+
+		echo "Skill $i " . $skills[$i] . "<br>";
+	}
+
+	
+}
+?>
---
+++ IS218/register.php
@@ -0,0 +1,109 @@
+
+<nav>
+    <ul>     
+        <li>
+            <a href="index.php">Home</a>
+        </li>
+        <li>
+            <a href="login.php">Login</a>
+        </li>
+        <li>
+            <a href="forums.php">Questions</a>
+        </li>
+    </ul>
+</nav>
+
+<h4>Register</h4>
+<form method="POST">
+	
+	<label for="first_name">First Name
+	<input type="first_name" id="first_name" name="first_name" autocomplete="off" />
+	</label>
+	
+	<label for="last_name">Last Name
+	<input type="last_name" id="last_name" name="last_name" autocomplete="off" />
+	</label>
+
+	<label for="birthday">Birthday
+	<input type="birthday" id="birthday" name="birthday" autocomplete="off" />
+	</label>
+
+	<label for="email">Email
+	<input type="email" id="email" name="email" autocomplete="off" />
+	</label>
+
+	<label for="p">Password
+	<input type="password" id="p" name="password" autocomplete="off"/>
+	</label>
+	
+	<input type="submit" name="register" value="Register"/>
+</form>
+
+<?php
+
+
+if(isset($_POST["register"])){
+	if(isset($_POST["password"]) && isset($_POST["email"]) && isset($_POST["first_name"]) && isset($_POST["last_name"]) && isset($_POST["birthday"])  ){
+		$password = $_POST["password"];
+		$email = $_POST["email"];
+		$firstName = $_POST["first_name"];
+		$lastName = $_POST["last_name"];
+		$birthday = $_POST["birthday"];
+			
+			$errors = [];
+			if(!strrpos ($email , '@' ) )
+			{
+
+				array_push($errors, "Invalid email");
+
+			}
+			
+			if(empty($email)){
+
+				array_push($errors, "Must enter email");
+
+			}
+
+			if(empty($firstName)){
+
+				array_push($errors, "Must enter your first name");
+
+			}
+
+				if(empty($lastName)){
+
+				array_push($errors, "Must enter your last name");
+
+			}
+
+				if(empty($birt)){
+
+				array_push($errors, "Must enter your birthday");
+
+			}
+
+			if(empty($password)){
+	
+				array_push($errors, "Must enter Password");
+			}
+
+			if(strlen($password) <= 8){
+
+				array_push($errors, "Password must be 8 or more characters");
+			}
+			
+			if(count($errors) > 0){
+
+				echo var_export($errors);
+				die();
+
+			}
+	}
+
+	echo $firstName . "<br>";
+	echo $lastName . "<br>";
+	echo $birthday . "<br>";
+	echo $email . "<br>";
+	echo $password . "<br>";
+}
+?>
